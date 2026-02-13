9808e8c8a99e2f10950e544e4bd1fee1ff9ec6ad
ejakait <ejakaitcaleb@gmail.com>
2026-02-13 17:15:12 +0300

    feat: Add metacrawler and tui entrypoints
    This commit introduces the main entrypoint for the `metacrawler` CLI
    tool
    and a basic Terminal User Interface (TUI) using Bubble Tea. The
    `metacrawler`
    entrypoint initializes logging, loads environment variables, and starts
    the
    crawling process. The TUI provides a simple interactive menu for
    demonstration
    purposes.

---
+++ cmd/metacrawler/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"github.com/ejakait/meta_crawler/crawler"
+	logging "github.com/ejakait/meta_crawler/internal/logging"
+	storage "github.com/ejakait/meta_crawler/internal/storage"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	var err error
+
+	err = godotenv.Load()
+	if err != nil {
+		slog.Error("Failed to load .env file", "error", err)
+		os.Exit(1)
+	}
+	env := os.Getenv("env")
+	GOOGLE_CREDENTIALS_JSON := os.Getenv("GOOGLE_CREDENTIALS_JSON")
+	if env == "prod" {
+		logging.InitLogger(true)
+	} else {
+		logging.InitLogger(false)
+	}
+	ctx := context.Background()
+
+	slog.Info("Application Started")
+	storage.InitDB(ctx, "meta.db")
+	config := make(map[string]string)
+	config["project_id"] = "vbcdata"
+	config["json_config"] = string(GOOGLE_CREDENTIALS_JSON)
+	err = crawler.StartCrawl(ctx, config)
+	if err != nil {
+		slog.Error("failed to start crawl", "error", err)
+		return
+	}
+	// containerList, err := storage.ListGCSContainers("google")
+
+	// slog.Info("containers found", "count", len(containerList))
+	// if err != nil {
+	// 	slog.Error("failed to list files", "error", err)
+
+	// }
+
+	// containerItems, err := storage.ListContainerItems("pharmaccess-cs")
+	// if err != nil {
+	// 	slog.Error("failed to list container items", "error", err)
+	// }
+	// for _, item := range containerItems {
+	// 	size, _ := item.Size()
+	// 	rawReader := &storage.StowReaderAt{Item: item}
+
+	// 	bufferedReader := bufra.NewBufReaderAt(rawReader, 1024*1024)
+
+	// 	pf, err := parquet.OpenFile(bufferedReader, size)
+
+	// 	if err != nil {
+	// 		slog.Error("failed to open parquet file", "error", err)
+	// 		continue
+	// 	}
+
+	// 	slog.Info("file found", "name", item.Name(), "size", size, "rows", pf.NumRows(), "columns", len(pf.Schema().Fields()))
+	// }
+}
